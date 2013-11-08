3a1afd403a425f956aa4cabdbf0b99e6290c4b89
Stephan Miller <stephanmil@gmail.com>
2013-11-07 22:24:29 -0600

    Load the ini file in the bootstrap

--- public/index.php
+++ public/index.php
@@ -3,6 +3,10 @@
 		//Create a DI
 		$di = new Phalcon\DI\FactoryDefault();
 
+		//Load ini file
+		$config = new \Phalcon\Config\Adapter\Ini('../app/config/application.ini');
+    	$di->set('config', $config);
+    	
 		//Set up our views
 		$di->set(’view’, function(){
 			$view = new \Phalcon\Mvc\View();
