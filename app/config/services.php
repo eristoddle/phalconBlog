50815d321f44c6bf10c76124f98459f7208a729f
Stephan Miller <stephanmil@gmail.com>
2013-11-10 15:30:15 -0600

    Chapter 2 First Draft

--- app/config/services.php
+++ app/config/services.php
@@ -1,5 +1,4 @@
 <?php
-
 use Phalcon\DI\FactoryDefault,
 	Phalcon\Mvc\View,
 	Phalcon\Mvc\Url as UrlResolver,
@@ -7,12 +6,10 @@ use Phalcon\DI\FactoryDefault,
 	Phalcon\Mvc\View\Engine\Volt as VoltEngine,
 	Phalcon\Mvc\Model\Metadata\Memory as MetaDataAdapter,
 	Phalcon\Session\Adapter\Files as SessionAdapter;
-
 /**
  * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
  */
 $di = new FactoryDefault();
-
 /**
  * The URL component is used to generate all kind of urls in the application
  */
@@ -21,7 +18,6 @@ $di->set('url', function() use ($config) {
 	$url->setBaseUri($config->application->baseUri);
 	return $url;
 }, true);
-
 /**
  * Setting up the view component
  */
@@ -48,7 +44,6 @@ $di->set('view', function() use ($config) {
 
 	return $view;
 }, true);
-
 /**
  * Database connection is created based in the parameters defined in the configuration file
  */
@@ -60,14 +55,12 @@ $di->set('db', function() use ($config) {
 		'dbname' => $config->database->dbname
 	));
 });
-
 /**
  * If the configuration specify the use of metadata adapter use it or use memory otherwise
  */
 $di->set('modelsMetadata', function() {
 	return new MetaDataAdapter();
 });
-
 /**
  * Start the session the first time some component request the session service
  */
