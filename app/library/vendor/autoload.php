46e0131926ae681ec6fc20a8469da95927f32747
Stephan Miller <stephanmil@gmail.com>
2013-11-11 11:26:32 -0600

    update

--- app/library/vendor/autoload.php
+++ app/library/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit9c9e7b5d30eed2741ee4a842e6216335::getLoader();
+return ComposerAutoloaderInit7e48117a03a354273fa2203be8cc117d::getLoader();
