76ec67bb9ab2366d917e5e0e6e4a1a8e36344947
Stephan Miller <stephanmil@gmail.com>
2013-11-13 19:04:01 -0600

    relative webtools path

--- public/webtools.config.php
+++ public/webtools.config.php
@@ -1,4 +1,4 @@
 <?php
-define('PTOOLSPATH', '/htdocs/phalconBlog/app/library/vendor/phalcon/devtools');
+define('PTOOLSPATH', str_replace('/public','',getcwd()).'/app/library/vendor/phalcon/devtools');
 /* you can set ADMINIP as IP 192.168.0.1 or SUBNET 192. or 10.0.2. or 86.84.124. */
 define('ADMINIP', '192.168.');
