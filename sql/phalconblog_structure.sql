03c632f15dc36b238a3a7f0911c715a5b819222b
Stephan Miller <stephanmil@gmail.com>
2013-12-07 19:03:09 -0600

    data model changes

--- sql/phalconblog_structure.sql
+++ sql/phalconblog_structure.sql
@@ -1,123 +1,142 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+-- phpMyAdmin SQL Dump
+-- version 3.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 07, 2013 at 05:17 PM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.9-4ubuntu2.3
 
-CREATE SCHEMA IF NOT EXISTS `phalconblog` DEFAULT CHARACTER SET latin1 ;
-USE `phalconblog` ;
-
--- -----------------------------------------------------
--- Table `phalconblog`.`users`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `phalconblog`.`users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `username` VARCHAR(16) NOT NULL ,
-  `password` VARCHAR(255) NOT NULL ,
-  `name` VARCHAR(255) NOT NULL ,
-  `email` TEXT NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `phalconblog`.`posts`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `phalconblog`.`posts` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `user_id` INT(11) NOT NULL ,
-  `title` TEXT NULL DEFAULT NULL ,
-  `body` TEXT NULL DEFAULT NULL ,
-  `excerpt` TEXT NULL DEFAULT NULL ,
-  `published` DATETIME NULL DEFAULT NULL ,
-  `updated` DATETIME NULL DEFAULT NULL ,
-  `pinged` TEXT NULL DEFAULT NULL ,
-  `to_ping` TEXT NULL DEFAULT NULL ,
-  `users_id` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_posts_users` (`users_id` ASC) ,
-  CONSTRAINT `fk_posts_users`
-    FOREIGN KEY (`users_id` )
-    REFERENCES `phalconblog`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 13
-DEFAULT CHARACTER SET = utf8;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `phalconblog`
+--
 
--- -----------------------------------------------------
--- Table `phalconblog`.`comments`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `phalconblog`.`comments` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `post_id` INT(11) NOT NULL ,
-  `body` TEXT NOT NULL ,
-  `name` TEXT NOT NULL ,
-  `email` TEXT NOT NULL ,
-  `url` TEXT NOT NULL ,
-  `submitted` DATETIME NOT NULL ,
-  `publish` TINYINT(1) NOT NULL ,
-  `posts_id` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_comments_posts1` (`posts_id` ASC) ,
-  CONSTRAINT `fk_comments_posts1`
-    FOREIGN KEY (`posts_id` )
-    REFERENCES `phalconblog`.`posts` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `comments`
+--
 
--- -----------------------------------------------------
--- Table `phalconblog`.`config`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `phalconblog`.`config` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `config` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL,
+  `body` text NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `url` text NOT NULL,
+  `submitted` datetime NOT NULL,
+  `publish` tinyint(1) NOT NULL,
+  `posts_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_comments_posts1` (`posts_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `phalconblog`.`tags`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `phalconblog`.`tags` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `tag` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+--
+-- Table structure for table `config`
+--
 
+CREATE TABLE IF NOT EXISTS `config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `config` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- -----------------------------------------------------
--- Table `phalconblog`.`post_tags`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `phalconblog`.`post_tags` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `post_id` INT(11) NOT NULL ,
-  `tag_id` INT(11) NOT NULL ,
-  `tags_id` INT(11) NOT NULL ,
-  `posts_id` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `fk_post_tags_tags1` (`tags_id` ASC) ,
-  INDEX `fk_post_tags_posts1` (`posts_id` ASC) ,
-  CONSTRAINT `fk_post_tags_tags1`
-    FOREIGN KEY (`tags_id` )
-    REFERENCES `phalconblog`.`tags` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_post_tags_posts1`
-    FOREIGN KEY (`posts_id` )
-    REFERENCES `phalconblog`.`posts` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `posts`
+--
 
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `title` text,
+  `body` text,
+  `excerpt` text,
+  `published` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `pinged` text,
+  `to_ping` text,
+  PRIMARY KEY (`id`),
+  KEY `fk_posts_users` (`users_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_tags`
+--
+
+CREATE TABLE IF NOT EXISTS `post_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `posts_id` int(11) NOT NULL,
+  `tags_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_post_tags_tags1` (`tags_id`),
+  KEY `fk_post_tags_posts1` (`posts_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `fk_comments_posts1` FOREIGN KEY (`posts_id`) REFERENCES `posts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `fk_posts_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `post_tags`
+--
+ALTER TABLE `post_tags`
+  ADD CONSTRAINT `fk_post_tags_tags1` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_post_tags_posts1` FOREIGN KEY (`posts_id`) REFERENCES `posts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
